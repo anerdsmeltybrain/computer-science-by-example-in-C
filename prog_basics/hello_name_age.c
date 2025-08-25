f57cb118fd34485992be5c273ebbf2c3830ecf2f
jason dawn <hdviews.01@gmail.com>
2025-08-25 16:55:23 -0400

    I'm don't with that file for now

--- prog_basics/hello_name_age.c
+++ prog_basics/hello_name_age.c
@@ -10,18 +10,20 @@ int main(int argc, char *argv[]) {
         char named_input[30];
 
         if (argc == 1) {
-                scanf("%s %d", &named_input[0], &age);
+                scanf("%s %d", named_input, &age);
 
-                if( sizeof(name) > 30 ) {
+                if( sizeof(named_input) > 30 ) {
                         printf("please enter a name that is less than 30 characters\n");
                 }
                 
                 printf("%s is %d years old\n", named_input, age);
+
         } else if ( argc == 3 ) {
                 name = argv[1];
                 age = atoi(argv[2]);
                 printf("%s is %d years old\n", name, age);
                 exit(0);
+
         } else {
                 printf("you entered the commands wrong\n");
                 printf("it's <program_name> <name> <age>\n");
